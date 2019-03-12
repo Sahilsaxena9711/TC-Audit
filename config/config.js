1a96b195cfc2a6a229d45085537d237fb366e0af
Sahilsaxena9711 <sahilsaxena9711@gmail.com>
2019-03-12 11:46:37 +0530

    changes in port

--- config/config.js
+++ config/config.js
@@ -5,10 +5,13 @@ if (env == "development") {
     process.env.PORT = process.env.PORT || 8080;
     process.env.MONGODB_URI = 'mongodb://Sahil9711:Sahil1058@ds235180.mlab.com:35180/audit';
 } else if (env == "production") {
-    process.env.PORT = process.env.PORT || 8080;
+    console.log(process.env.PORT);
+    
+    process.env.PORT = process.env.PORT;
     process.env.MONGODB_URI = 'mongodb://Sahil9711:Sahil1058@ds235180.mlab.com:35180/audit';
 } else {
-    process.env.PORT = process.env.PORT || 8080;
+    console.log(process.env.PORT);
+    process.env.PORT = process.env.PORT;
     process.env.MONGODB_URI = 'mongodb://Sahil9711:Sahil1058@ds235180.mlab.com:35180/audit';
 }
 
