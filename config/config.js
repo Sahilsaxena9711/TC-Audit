d70192bba12ff5b94a923bb1f6798170fb713284
Sahilsaxena9711 <sahilsaxena9711@gmail.com>
2019-03-12 12:28:49 +0530

    changes in config

--- config/config.js
+++ config/config.js
@@ -2,16 +2,14 @@ var env = process.env.NODE_ENV;
 console.log(`Enviornment :: ${env}`);
 
 if (env == "development") {
-    process.env.PORT = process.env.PORT || 8080;
+    process.env.PORT = process.env.PORT || 8010;
     process.env.MONGODB_URI = 'mongodb://Sahil9711:Sahil1058@ds235180.mlab.com:35180/audit';
 } else if (env == "production") {
     console.log(process.env.PORT);
-    
-    process.env.PORT = process.env.PORT;
+    process.env.PORT = process.env.PORT || 8010;
     process.env.MONGODB_URI = 'mongodb://Sahil9711:Sahil1058@ds235180.mlab.com:35180/audit';
 } else {
-    console.log(process.env.PORT);
-    process.env.PORT = process.env.PORT;
+    process.env.PORT = process.env.PORT || 8010;
     process.env.MONGODB_URI = 'mongodb://Sahil9711:Sahil1058@ds235180.mlab.com:35180/audit';
 }
 
